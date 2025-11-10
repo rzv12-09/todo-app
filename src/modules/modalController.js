5297b497dfe027adfa2ec957f1148451dff03490
rzv12-09 <olariurazvan20@gmail.com>
2025-11-10 21:40:34 +0200

    feat: add priority selection when creating a new task

--- src/modules/modalController.js
+++ src/modules/modalController.js
@@ -67,6 +67,38 @@ const ModalController = (()=>{
             dialog.close();
         })
 
+        const priorityComboBox = document.createElement("div");
+        const labelPriority = document.createElement("label");
+        labelPriority.for = "priority-select";
+        priorityComboBox.appendChild(labelPriority);
+        const selectPriority = document.createElement("select");
+        selectPriority.name = "priority";
+        selectPriority.id = "priority-select";
+        const option1 = document.createElement("option");
+        option1.textContent = "Priority 1"
+        option1.value = "1";
+
+        const option2 = document.createElement("option");
+        option2.textContent = "Priority 2"
+        option2.value = "2";
+
+        const option3 = document.createElement("option");
+        option3.textContent = "Priority 3"
+        option3.value = "3";
+
+        const option4 = document.createElement("option");
+        option4.textContent = "Priority 4"
+        option4.value = "4";
+        option4.selected = true;
+
+
+        selectPriority.appendChild(option1);
+        selectPriority.appendChild(option2);
+        selectPriority.appendChild(option3);
+        selectPriority.appendChild(option4);
+
+        priorityComboBox.appendChild(selectPriority);
+
         const addBtn = document.createElement("button");
         addBtn.textContent = "Add task";
         addBtn.addEventListener("click",()=>{
@@ -75,6 +107,7 @@ const ModalController = (()=>{
             const desc = descriptionInput.value;
             const dueDate = dueDateInput.value;
             const projectObj = ProjectController.findProjectById(projectId);
+            const priority = selectPriority.value;
             projectObj.addTask(name,desc,dueDate,"none");
 
             if(projectId === ContentController.getActiveProject().getId())
@@ -87,6 +120,7 @@ const ModalController = (()=>{
         dialog.appendChild(descriptionInput);
         dialog.appendChild(dueDateInput);
         dialog.appendChild(projectComboBox);
+        dialog.appendChild(priorityComboBox);
         dialog.appendChild(cancelBtn);
         dialog.appendChild(addBtn);
         dialog.showModal();
