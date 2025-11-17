547abf171df6f9e471550d1ea754b50aadebc0ce
rzv12-09 <olariurazvan20@gmail.com>
2025-11-17 11:18:38 +0200

    feat: restore projects from localStorage when page loads

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import "./style.css";
 import ProjectController from "./modules/projectController.js";
 import SidebarController from "./modules/sidebarController.js";
 import ContentController from "./modules/contentController.js";
+import StorageController from "./modules/storageController.js";
 
 window.ProjectController = ProjectController;
 window.SidebarController = SidebarController;
+StorageController.restoreProjects();
 SidebarController.render();
 SidebarController.bindEvents();
 ContentController.renderProjectTasks(ProjectController.getProjects()[0]);
--- src/modules/storageController.js
+++ src/modules/storageController.js
@@ -8,8 +8,32 @@ const StorageController = (()=>{
         localStorage.setItem("projects",JSON.stringify(projectList));
     }
 
+    const restoreProjects = () =>{
+        const localProjects = JSON.parse(localStorage.getItem("projects"));
+        localProjects.forEach((project,index) => {
+            const name = project.name;
+            const tasks = project.taskList;
+            console.log(tasks);
+            if(index != 0)
+                ProjectController.addProject(name);
+            const currentProject = ProjectController.getProjects()[index];
+
+            tasks.forEach((task,index2) => {
+                   
+                  currentProject.addTask(task.title,task.description,task.dueDate,task.priority);  
+                  if(task.completed){
+                    currentProject.getTaskList()[index2].toggle();
+                  }
+
+            });{
+             
+            }
+        });
+    }
+
     return{
         storeProjects,
+        restoreProjects,
     }
 })();
 
